d0685c83d86a5b9cd54ef3d811e69f364b3f9b89
kunlun zhang <zhangkunlun01@inspur.com>
2024-09-03 11:09:08 +0800

    feat: 优化模板代码

--- src/router/open.ts
+++ src/router/open.ts
@@ -1,4 +1,3 @@
-import userController from "../controllers/user";
 import siteController from "../controllers/site"
 import koaRouter from "koa-router";
 import { methodType } from "./enum"
@@ -6,30 +5,8 @@ import { methodType } from "./enum"
 const router = new koaRouter();
 /** 开放接口，不进行校验 */
 const type = "/open";
-const service = "/user"
-
 
 const routerList = [
-  {
-    path: `${type}/user/info`,
-    method: methodType.POST,
-    action: userController.getUser,
-  },
-  {
-    path: `${type}/user/insert`,
-    method: methodType.POST,
-    action: userController.insertUser,
-  },
-  {
-    path: `${type}/user/delete`,
-    method: methodType.POST,
-    action: userController.deleteUser,
-  },
-  {
-    path: `${type}/user/update`,
-    method: methodType.POST,
-    action: userController.updateUser,
-  },
   /** 获取网站信息 */
   {
     path: `${type}/site/info`,
