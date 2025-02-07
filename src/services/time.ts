49af8bb023dce16b8a9b9e30a932c2635e8e5045
kunlun zhang <zhangkunlun01@inspur.com>
2025-02-07 10:37:12 +0800

    feat: 日期接口更新

--- src/services/time.ts
+++ src/services/time.ts
@@ -6,31 +6,31 @@ export default class TimeService {
     async getNextHolidayInfo(currentDay:string) {
         const cl = db.collection('holiday-info');
         
+        // 查询当前日期的index
+        const curDay = await cl.findOne({
+            date: currentDay,
+        },{
+            projection: { index: 1, _id: 0 }
+        })
 
         // 查询条件：日期大于等于当前日期，且stateNum为2
         const query = {
             date: { $gte: currentDay },
             stateNum: "2" // 法定节假日
         }
-        // 按日期升序排序
+        // 按日期升序排序，查询第一个
         const nextHoliday = await cl.findOne(query, {
             sort: { date: 1 },
-            projection: { date: 1,stateText:1,_id:0 }
+            projection: { date: 1,index:1,stateText:1,_id:0 }
         })
         // 将当前日期转换为整数，计算日期
-        const currentDayInt = parseInt(currentDay, 10);
-        const diffDays = nextHoliday?.date - currentDayInt
+        const currentDayIndex = parseInt(curDay?.index, 10);
+        const diffDays = nextHoliday?.index - currentDayIndex
         
-        if (diffDays === 0){
-            return `今天是【${nextHoliday?.stateText}】,假期愉快~`
-        } else if (diffDays === 1){
-            return `加油！明天就是【${nextHoliday?.stateText}】了！`
-        } else if (diffDays === 2){
-            return `再坚持一下！后天就是【${nextHoliday?.stateText}】了！`
-        } else {
-            return `距离【${nextHoliday?.stateText}假期】还有【${diffDays}】天`
+        return {
+            holiday: nextHoliday?.stateText,
+            days: diffDays
         }
-
     }
 
     /** 获取下个周末 */
